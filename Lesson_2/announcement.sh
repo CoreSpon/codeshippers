8b5ffb4667c5666b9c172f1632f482b3286fa093
Hawaiideveloper <coreymalbright@gmail.com>
2017-10-01 17:48:01 -0400

    Lesson 3

--- Lesson_2/announcement.sh
+++ Lesson_2/announcement.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+#clear all junk off screen
+clear 
+
+#say something on screen showing your progress
 echo "you are now in lesson 2"
+echo "and you properly have sourced a file from a previous script"
---
+++ Lesson_3/if_statement.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+if [  -f ./if_statement.sh ]; then
+    echo "File  found!"
+fi
